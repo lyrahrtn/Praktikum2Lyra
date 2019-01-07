5f27035b2b06ce8141824ebe3597c8aef8bcd7b0
Lyra Hertin <Lyra Hertin>
2019-01-07 14:17:44 +0700

    selesai

---
+++ latihan7.php
@@ -0,0 +1,18 @@
+<?php
+
+$arrBuah = array ("Mangga", "Apel", "Pisang", "Jeruk");
+echo $arrBuah[0]; //Mangga
+echo "<br>";
+echo $arrBuah[3]; //Jeruk
+echo "<br>";
+
+$arrWarna = array();
+$arrWarna[] = "Merah";
+$arrWarna[] = "Biru";
+$arrWarna[] = "Hijau";
+$arrWarna[] = "Putih";
+echo $arrWarna[0]; //Merah
+echo "<br>";
+echo $arrWarna[2]; //Hijau
+
+?>
